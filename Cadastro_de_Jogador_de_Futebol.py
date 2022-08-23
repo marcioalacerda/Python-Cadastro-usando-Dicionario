341d0dcaad5b77d8b1a4d9ad8c3a9bba8973ed21
Marcio Lacerda <marcioalacerda2201@gmail.com>
2022-08-23 10:11:33 -0300

    Aprimorando o programa para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador

--- Cadastro_de_Jogador_de_Futebol.py
+++ Cadastro_de_Jogador_de_Futebol.py
@@ -1,28 +1,55 @@
-'''Programa que gerencie o aproveitamento de um jogador de futebol.
-O programa vai ler o nome do jogador e quantas partidas ele jogou.
-depois vai ler a quantidade de gols feito em cada partida.
-No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.'''
+'''Aprimorando o programa para que ele funcione com vários jogadores, incluindo um sistema de visualização
+de detalhes do aproveitamento de cada jogador'''
 from time import sleep
 dic = dict()
 lista = list()
-dic['nome'] = str(input('Nome: ')).strip().capitalize()
-quant = int(input(f'Quantas partidas {dic["nome"]} jogou? '))
-for c in range(0, quant):
-    lista.append(int(input(f'Quantos gols na partida {c + 1}? ')))
-dic['gols'] = lista[:]
-dic['total'] = sum(lista) # somando os elementos da lista
+campeonato = list()
+while True:
+    print('-'*40)
+    dic['nome'] = str(input('Nome do Jogador: ')).strip().capitalize()
+    quant = int(input(f'Quantas partidas {dic["nome"]} jogou? '))
+    for c in range(0, quant):
+        lista.append(int(input(f'Quantos gols na partida {c + 1}? ')))
+    dic['gols'] = lista[:]
+    dic['total'] = sum(lista)
+    lista.clear()
+    campeonato.append(dic.copy())
+    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    while resp not in "SN":
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
 sleep(1)
 print('-='*20)
-print(dic)
-print('-='*20)
-for k, v in dic.items():
+#pode ser feito desta maneira também
+#print(f'{"Cod":<4}{"Nome":<10}{"Gols":<20}{"Total":>6}')
+print('Cod ', end='')
+for i in dic.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-'*40)
+for i, v in enumerate(campeonato):
     sleep(1)
-    print(f'O campo {k} tem o valor {v}.')
-sleep(1)
-print(f'O {dic["nome"]} jogou {quant} partida(s).')
-for c in range(0, quant):
-    sleep(1)
-    print(f'  => Na partida {c+1}, fez {lista[c]} gol(s).')
-print(f'Foi um total de {dic["total"]} gol(s).')
-print('-='*20)
-print('<<< ENCERROU >>>')
+    # pode ser feito desta maneira também
+    #print(f'{i:<4}{campeonato[i]["nome"]:<10}{campeonato[i]["gols"]}{campeonato[i]["total"]:>19}')
+    print(f'{i:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-' * 40)
+while True:
+    pesquisa = int(input('Mostrar dados de qual jogador? (Digite o Cod.)(para sair 999) '))
+    while pesquisa > len(campeonato)-1 and pesquisa != 999:
+        print(f'ERRO! Não existe jogador com o codigo {pesquisa}! Tente novamente.')
+        print('-' * 40)
+        pesquisa = int(input('Mostrar dados de qual jogador? (Digite o Cod.)(para sair 999) '))
+    if pesquisa == 999:
+        break
+    print(f'-LEVANTAMENTO DO JOGADOR {campeonato[pesquisa]["nome"]}-')
+    print(f'O {campeonato[pesquisa]["nome"]} jogou {len(campeonato[pesquisa]["gols"])} partida(s).')
+    for i, g in enumerate(campeonato[pesquisa]['gols']):
+        sleep(1)
+        print(f'  => Na partida {i+1}, fez {g} gol(s).')
+    print(f'Foi um total de {campeonato[pesquisa]["total"]} gol(s).')
+    print('-='*20)
+print('<<< VOLTE SEMPRE >>>')
